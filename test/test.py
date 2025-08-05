74a0ee17fcc560a47ec0f95a0a31105d63e07ed5
Ark <mbawazir951@gmail.com>
2025-08-05 09:57:28 +0300

    v2-features

---
+++ test/test.py
@@ -0,0 +1,17 @@
+
+col = ["A","B","C"]
+row = [
+    ["TEST", "TEST", "TEST"],
+    ["TEST", "TEST", "TEST"],
+    ["TEST", "TEST", "TEST" ],
+]
+
+# YOUR CODE:
+
+
+
+
+
+# ------------------------>
+
+print("ended")
